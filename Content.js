ddab7d410da0518c71e6f57011d1d9de2aeee8fb
Ilja H <ilja200303@gmail.com>
2022-02-16 15:07:45 +0200

    check if last page

--- Content.js
+++ Content.js
@@ -1,11 +1,14 @@
 function getContent(){
     var index = 3
     var question_title_array = []
-    while (index <= 12){
+    var node = document.querySelectorAll('[title="Ava küsimus"]');
+
+    while (index <= node.length + 2){
         var question_title = document.evaluate(`/html/body/div[2]/div[1]/div[2]/div[2]/div[2]/div/div[${index}]/div[1]/div/div/div/div/div/div/div/div/div[1]/h2/span[1]/a`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
         question_title_array.push(question_title.textContent)
         index++
     }
+
     for (var i = 0; i < question_title_array.length; i++){
         console.log(question_title_array[i])
     }
@@ -13,14 +16,14 @@ function getContent(){
 
 
 function getCategory(){
-    var index = 1
+    var index = 11
     if(localStorage.getItem("category_index")){
         index = localStorage.getItem("category_index")
     }else{
         localStorage.setItem("category_index", index)
         localStorage.setItem("nextCategory", true.toString())
     }
-
+    // /html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div/div/div/div/div/div/div/ul/li[2]/ul/li[${index}]/a
     var category_name = document.evaluate(`/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div/div/div/div/div/div/div/ul/li[2]/ul/li[${index}]/a`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     if (localStorage.getItem("nextCategory") === "true"){
         category_name.click()
@@ -44,43 +47,39 @@ function getNextPage(){
     var until = 6
 
     if (number_of_page < until){
-        until = 4
+        until = 3
     }
 
     console.log(number_of_page)
 
+
     setTimeout(function() {
         var page = document.evaluate(`html/body/div[2]/div[1]/div[2]/div[2]/div[2]/div/div[2]/a[${index}]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
 
-        if (parseInt(page.textContent) <= number_of_page){
-            page.click()
-        }
-
-        if(parseInt(page.textContent) === number_of_page){
+        if (page === null){
             category_index++
             localStorage.setItem("category_index", category_index)
             localStorage.setItem("nextCategory", true.toString())
             getCategory()
+            localStorage.removeItem("index")
         }
-
+        else {
+            page.click()
+        }
 
         if(index === 1){
             index++
         }
         index++
 
-        if (index <= 6) {
+        if (index <= until) {
             localStorage.setItem("index", index)
 
-            getNextPage(index);
+            getNextPage();
         }
-        else if(parseInt(page.textContent) === number_of_page){
-            localStorage.removeItem("index")
-        }
-
-
 
     }, 5000)
+
 }
 
 function main(){
